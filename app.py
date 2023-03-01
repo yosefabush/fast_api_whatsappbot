c89f361c756b5d4f8245ac9b195e7b6970c8da79
yosefabush <yosef0525@gmail.com>
2023-03-01 17:07:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -231,5 +231,5 @@ def check_if_session_exist(user_id):
 
 
 if __name__ == "__main__":
-    # uvicorn.run(app, host="0.0.0.0", port=8000)
-    uvicorn.run(app, host="127.0.0.1", port=int(PORT))
+    uvicorn.run(app, host="0.0.0.0", port=int(PORT))
+    # uvicorn.run(app, host="127.0.0.1", port=int(PORT))
