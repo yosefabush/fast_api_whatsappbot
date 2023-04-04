c015b1912aa417f37da46453dfacec10b1aab1e5
yosefabush <yosef0525@gmail.com>
2023-04-04 12:47:54 +0300

    product name

--- app/app.py
+++ app/app.py
@@ -375,7 +375,7 @@ def process_bot_response(db, user_msg: str, button_selected=False) -> str:
                 summary = json.loads(session.convers_step_resp)
                 client_id = session.password.split(";")[1]
                 _phone_number_with_0 = summary['5'].replace('972', '0')
-                kria_header = f"מספר מדבקה: {summary['4']}" if summary['4'].isdigit() else f"מספר מוצר: {summary['4']}"
+                kria_header = f"מספר מדבקה: {summary['4']}" if summary['4'].isdigit() else f"שם מוצר: {summary['4']}"
                 data = {"technicianName": f"{_phone_number_with_0} {summary['6']}",
                         # product name and phone
                         "kria": f"{kria_header}\nהמספר ממנו נפתחה הקריאה: {session.user_id.replace('972', '0')}",
